ac14c68901bfba710a5babe4272e9222e3f113af
YongZhang <zy0306@foxmail.com>
2026-08-21 09:30:23 +0800

    支持指定执行skill&系统提示词

--- examples/agent_service/bocomadp/routers/__init__.py
+++ examples/agent_service/bocomadp/routers/__init__.py
@@ -21,6 +21,7 @@ from .credential_model import credential_model_router
 from .oss_download import oss_download_router
 from .session_usage import session_usage_router
 from .agent_tools import agent_tools_router
+from .system_prompt import system_prompt_router
 
 __all__ = [
     "models_router",
@@ -32,4 +33,5 @@ __all__ = [
     "oss_download_router",
     "session_usage_router",
     "agent_tools_router",
+    "system_prompt_router",
 ]
